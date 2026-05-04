cae555e45a23f88cb3aad54c6e0fa310dda5106f
maeta187 <nexus18789@gmail.com>
2026-05-04 14:43:34 +0900

    🔧 : YamadaUIの初期設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
+import { ColorModeScript, UIProvider } from '@/components/ui';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
+// const geistSans = Geist({
+//   variable: '--font-geist-sans',
+//   subsets: ['latin'],
+// });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+// const geistMono = Geist_Mono({
+//   variable: '--font-geist-mono',
+//   subsets: ['latin'],
+// });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -23,8 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <ColorModeScript />
+        <UIProvider>{children}</UIProvider>
+      </body>
     </html>
   );
 }
