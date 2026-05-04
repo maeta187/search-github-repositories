5d1a3e54f90bc29d699afe6b8c946b7768621d05
maeta187 <nexus18789@gmail.com>
2026-05-04 23:04:17 +0900

    ✨ : Footerコンポーネントの実装

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,9 @@
+import { Container, Text } from '@/components/ui';
+
+export const Footer = () => {
+  return (
+    <Container.Footer paddingY={'lg'} paddingX={'lg'} borderTop={'sm'}>
+      <Text>Footer</Text>
+    </Container.Footer>
+  );
+};
