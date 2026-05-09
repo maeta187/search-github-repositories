a7f0ca80cf1a6cc1a12bc875ef634f393c837723
maeta187 <nexus18789@gmail.com>
2026-05-09 14:11:57 +0900

    ✨ : 定数の新規追加

--- src/constant/endpoint.ts
+++ src/constant/endpoint.ts
@@ -1,8 +1,13 @@
 const BASE_URL =
   process.env.NEXT_PUBLIC_API_ENDPOINT ?? 'http://localhost:3000';
 
-export const FIND_ONE_REPOSITORY_ENDPOINT = 'https://api.github.com/repos';
+export const GITHUB_API_ENDPOINT = 'https://api.github.com';
+
+export const SEARCH_REPOSITORIES_ENDPOINT = `${GITHUB_API_ENDPOINT}/search/repositories`;
+
+export const FIND_ONE_REPOSITORY_ENDPOINT = `${GITHUB_API_ENDPOINT}/repos`;
 
 export const API_ROUTES = {
   REPOSITORY_DETAIL: `${BASE_URL}/api/detail`,
+  SEARCH_REPOSITORIES: `${BASE_URL}/api/top`,
 } as const;
