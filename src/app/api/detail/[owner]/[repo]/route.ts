19e37db3abea0faa145f9bce3e78ee294cdefeed
maeta187 <nexus18789@gmail.com>
2026-05-18 18:31:40 +0900

    👔 : detail/APIルートでバリデーション実行するよう変更

--- src/app/api/detail/[owner]/[repo]/route.ts
+++ src/app/api/detail/[owner]/[repo]/route.ts
@@ -1,4 +1,8 @@
 import { fetchRepositoryDetail } from '@/features/detail/service';
+import {
+  validateRepositoryDetailOwnerParams,
+  validateRepositoryDetailRepoParams,
+} from '@/features/detail/validation';
 import { RepositoryDetailParams } from '@/types/detail';
 
 interface DetailRoutePrams {
@@ -8,6 +12,19 @@ interface DetailRoutePrams {
 export async function GET(_: Request, { params }: DetailRoutePrams) {
   try {
     const { owner, repo } = await params;
+
+    // オーナーのバリデーション
+    const isValidOwner = validateRepositoryDetailOwnerParams(owner);
+    if (isValidOwner instanceof Object && 'message' in isValidOwner) {
+      return Response.json({ error: isValidOwner.message }, { status: 400 });
+    }
+
+    // リポジトリー名のバリデーション
+    const isValidRepo = validateRepositoryDetailRepoParams(repo);
+    if (isValidRepo instanceof Object && 'message' in isValidRepo) {
+      return Response.json({ error: isValidRepo.message }, { status: 400 });
+    }
+
     const data = await fetchRepositoryDetail({ owner, repo });
     return Response.json(data);
   } catch (error) {
